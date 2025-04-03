cf071d9329482854cca7872d13fa91c7ba75c667
wh01s17 <jor.perezo@hotmail.com>
2025-04-03 20:22:08 -0300

    added search option

--- src/components/products/ProductsGrid.tsx
+++ src/components/products/ProductsGrid.tsx
@@ -5,6 +5,7 @@ import React, { useMemo, useState } from 'react'
 
 export const ProductsGrid = ({ products }: { products: Product[] }) => {
     const [sortOption, setSortOption] = useState("None")
+    const [search, setSearch] = useState("")
 
     const handleSortOption = (e: React.ChangeEvent<HTMLSelectElement>) => {
         e.preventDefault()
@@ -27,48 +28,74 @@ export const ProductsGrid = ({ products }: { products: Product[] }) => {
                 return productsCopy.sort((a, b) => b.price - a.price)
             case 'Top rated':
                 return productsCopy.sort((a, b) => b.rating.rate - a.rating.rate)
+            case 'Most reviewed':
+                return productsCopy.sort((a, b) => b.rating.count - a.rating.count)
             default:
                 return productsCopy
         }
     }, [products, sortOption])
 
+    const filterProducts = sortedProducts.filter(product => {
+        return product.title.toLowerCase().includes(search.toLowerCase()) ||
+            product.description.toLowerCase().includes(search.toLowerCase())
+    })
+
     if (!products) return null
 
     return (
         <section className='flex flex-col w-full justify-center my-20 gap-3'>
-            <div className=' flex flex-col pl-35 text-lg'>
-                <h1 className='font-bold'>Sort by</h1>
-                <select
-                    className='border w-fit rounded-lg px-1'
-                    value={sortOption}
-                    onChange={handleSortOption}
-                >
-                    <option>
-                        None
-                    </option>
-                    <option>
-                        Name: A to Z
-                    </option>
-                    <option>
-                        Name: Z to A
-                    </option>
-                    <option>
-                        Price: Low to High
-                    </option>
-                    <option>
-                        Price: High to Low
-                    </option>
-                    <option>
-                        Top rated
-                    </option>
-                </select>
+            <div className=' flex justify-between items-center px-35 text-lg'>
+                <div id='sort'>
+                    <h1 className='font-bold'>Sort by</h1>
+                    <select
+                        className='border w-fit rounded-lg px-1'
+                        value={sortOption}
+                        onChange={handleSortOption}
+                    >
+                        <option>
+                            None
+                        </option>
+                        <option>
+                            Name: A to Z
+                        </option>
+                        <option>
+                            Name: Z to A
+                        </option>
+                        <option>
+                            Price: Low to High
+                        </option>
+                        <option>
+                            Price: High to Low
+                        </option>
+                        <option>
+                            Top rated
+                        </option>
+                        <option>
+                            Most reviewed
+                        </option>
+                    </select>
+                </div>
+
+                <div id='search'>
+                    <input
+                        type="text"
+                        name="search"
+                        id="search"
+                        placeholder='Search...'
+                        className='border rounded-lg px-1'
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                </div>
             </div>
 
             <div className='flex flex-wrap w-full justify-center mb-20 gap-3'>
                 {
-                    sortedProducts.map(product => {
-                        return <CardProduct key={product.id} product={product} />
-                    })
+                    filterProducts.length > 0
+                        ? filterProducts.map(product => {
+                            return <CardProduct key={product.id} product={product} />
+                        })
+                        : <h1 className='text-2xl font-bold mt-20'>Apparently we don't have what you're looking for 😥</h1>
                 }
             </div>
         </section>
